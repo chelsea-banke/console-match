d25b1d1777a03869f4f1eba4ee800f44ade7b917
chelsea-banke <81103432+chelsea-banke@users.noreply.github.com>
2021-09-26 01:24:37 +0100

    initialisation
    initializing the Mentor class, Mentee class and matching function

---
+++ main.py
@@ -0,0 +1,22 @@
+# issue 1
+class Mentor:
+    def __init__(self):
+        pass
+
+
+# issue 2
+class Mentee:
+    def __init__(self):
+        pass
+
+
+# issue 3
+def match(mentorList, menteeList):
+    matches = {}
+    # matching logic
+    return matches
+
+
+if __name__ == '__main__':
+    # run and test the issues possible solutions
+    pass
